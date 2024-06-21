73e399f572bb1e1ad6d590249fc78d810883e1a7
yoyoyoda5949 <kimbabo134@naver.com>
2024-06-21 11:11:57 +0900

    Add files via upload

---
+++ hiphop.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+st.title("미국과 한국 래퍼들 모음집")
+st.sidebar.success("Select a page above.")
+
+if "my_input" not in st.session_state:
+    st.session_state["my_input"] = ""
+
+my_input = st.text_input("Input a text here", st.session_state["my_input"])
+submit = st.button("Submit")
+if submit:
+    st.session_state["my_input"] = my_input
+    st.write("You have entered: ", my_input)
